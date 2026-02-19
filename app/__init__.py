7e3a30755f8cbcab64c8d0b8be76218be482a6d7
manikshivam <manikshivam708@gmail.com>
2026-02-19 23:32:48 +0530

    add

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .extensions import db, login_manager
 from .auth.routes import auth_bp
 from .posts.routes import posts_bp
 from .games.routes import game
+from .core.routes import core
 from .models import User
 
 def create_app():
@@ -17,6 +18,7 @@ def create_app():
     app.register_blueprint(auth_bp)
     app.register_blueprint(posts_bp)
     app.register_blueprint(game)
+    app.register_blueprint(core)
     
     with app.app_context():
         db.create_all()
