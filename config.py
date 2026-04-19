f62cd8f17db015652b5cf0119c93e4d835f956f4
manikshivam <manikshivam708@gmail.com>
2026-04-19 16:10:55 +0530

    gunicorn

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # local only
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 
     DB_URL = os.getenv("DATABASE_URL")
 
-    # Fix Render/Supabase postgres format
     if DB_URL and DB_URL.startswith("postgres://"):
         DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = DB_URL or "sqlite:///site.db"
+    SQLALCHEMY_DATABASE_URI = DB_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "sslmode": "require"
+        }
+    }
