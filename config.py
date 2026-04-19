4d800529157114268d31b10d35461f29f5a520ee
manikshivam <manikshivam708@gmail.com>
2026-04-19 16:08:23 +0530

    gunicorn

--- config.py
+++ config.py
@@ -1,23 +1,16 @@
-# import os
-
-# class Config:
-#     SECRET_KEY = "supersecretkey"
-#     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 import os
 from dotenv import load_dotenv
 
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
-    
-    load_dotenv()
+load_dotenv()  # local only
 
-# Neon/PostgreSQL connection
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    
-    # MAIL_SERVER = os.getenv("MAIL_SERVER")
-    # MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
-    # MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
-    # MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    # MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    # MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+
+    DB_URL = os.getenv("DATABASE_URL")
+
+    # Fix Render/Supabase postgres format
+    if DB_URL and DB_URL.startswith("postgres://"):
+        DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = DB_URL or "sqlite:///site.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
